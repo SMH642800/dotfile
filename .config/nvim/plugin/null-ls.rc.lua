cb978e4ac719e5d6221a1d3ed22495c54c4a2e89
SMH642800 <SMH642800@gmail.com>
2022-11-22 23:33:04 +0800

    modify null_ls.lua

--- .config/nvim/plugin/null-ls.rc.lua
+++ .config/nvim/plugin/null-ls.rc.lua
@@ -3,27 +3,37 @@ if not status then
 	return
 end
 
+-- for conciseness
+local formatting = null_ls.builtins.formatting -- to setup formatters
+local diagnostics = null_ls.builtins.diagnostics -- to setup linters
+
+-- to setup format on save
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local lsp_formatting = function(bufnr)
 	vim.lsp.buf.format({
 		filter = function(client)
+			--  only use null-ls for formatting instead of lsp server
 			return client.name == "null-ls"
 		end,
 		bufnr = bufnr,
 	})
 end
 
+-- configure null_ls
 null_ls.setup({
+	-- setup formatters & linters
 	sources = {
-		null_ls.builtins.formatting.prettier,
-		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.diagnostics.eslint_d.with({
+		formatting.prettier,
+		formatting.stylua,
+		diagnostics.eslint_d.with({
 			diagnostics_format = "[eslint] #{m}\n(#{c})",
 		}),
-		null_ls.builtins.formatting.black,
-		null_ls.builtins.diagnostics.flake8,
+		formatting.black,
+		diagnostics.flake8,
 	},
+
+	-- configure format on save
 	on_attach = function(client, bufnr)
 		if client.supports_method("textDocument/formatting") then
 			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
